655b50c8086c348010fc676aaf8d90a4125b2c2a
mecastor <mecastor87@gmail.com>
2024-05-09 16:16:44 +0100

    Base routing and pages added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,30 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
+
 import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
